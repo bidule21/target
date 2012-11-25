94271a4930e833ebfbdc8441413745ea4b3237d2
Jade Robbins <jaderobbins@gmail.com>
2012-11-24 19:16:10 -0700

    Red and green alternate with 1000ms delays.

--- target.ino
+++ target.ino
@@ -1,19 +1,29 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
+  
  */
+int green = 9;
+int red = 10;
 
 void setup() {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(9, OUTPUT);     
+  pinMode(green, OUTPUT);
+  pinMode(red, OUTPUT);  
 }
 
 void loop() {
-  digitalWrite(9, HIGH);   // set the LED on
+  halt();
   delay(1000);              // wait for a second
-  digitalWrite(9, LOW);    // set the LED off
+  shoot();
   delay(1000);              // wait for a second
+}
+
+void shoot() {
+  digitalWrite(green, HIGH);
+  digitalWrite(red, LOW);
+}
+
+void halt() {
+  digitalWrite(red, HIGH);
+  digitalWrite(green, LOW);
 }
